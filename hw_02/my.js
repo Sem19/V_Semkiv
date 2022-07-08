f79e0dc236a36547e1ac9c521d2fe93d80ef372d
Sem19 <semkivvolodya@gmail.com>
2022-07-08 11:15:33 +0300

    changed the variable

--- hw_02/my.js
+++ hw_02/my.js
@@ -1,5 +1,4 @@
-let a = prompt('Enter the year of bith');
-const yearsOld = 2022 - Number(a);
+const yearsOld = 2022 - Number(prompt('Enter the year of bith'));
 const firstName = prompt('Enter your first name');
 const lastName = prompt('Enter your lasr name');
 
