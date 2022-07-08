8ef7d8433a8a94b0f74fb473ed209cf5bf47678d
Sem19 <semkivvolodya@gmail.com>
2022-07-08 11:20:41 +0300

    changed the veriable

--- hw_02/my.js
+++ hw_02/my.js
@@ -2,5 +2,6 @@ const yearsOld = 2022 - Number(prompt('Enter the year of bith'));
 const firstName = prompt('Enter your first name');
 const lastName = prompt('Enter your lasr name');
 
+
 console.log('User Bio : ' + firstName, lastName, + ',' + yearsOld, 'years old');
 alert('User Bio : ' + firstName + ' ' + lastName + ', ' + yearsOld + ' years old');
