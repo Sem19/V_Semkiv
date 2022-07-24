59238b9f6b183cfb41b347389cca6523288f4e5f
Sem19 <semkivvolodya@gmail.com>
2022-07-25 01:24:49 +0300

    fixed the second operation, and some conditions

--- hw_04/my.js
+++ hw_04/my.js
@@ -1,5 +1,12 @@
 const emoticon = ['😉', '😊','😃','😆','😉','🙂','🤬','🤢','☠','👻'];
 
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max); 
+
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 let randomString = String(prompt('Enter string'));
 let operation = Number(prompt(`Enter the transaction number: 
     1 unicode value;
@@ -8,28 +15,28 @@ let operation = Number(prompt(`Enter the transaction number:
     4 number of letters.
 `));
 
-if (operation == 1){
+if (operation === 1){
     let symbolIndex = Number(prompt('Enter symbol index'));
     console.log(randomString.charCodeAt(symbolIndex - 1));
 }
-else if (operation == 2){
+else if (operation === 2){
     let symbolIndex = Number(prompt('Enter symbol index'));
 
-    var symbol = randomString[symbolIndex - 1];
-    var re = new RegExp(symbol, 'g');
-
-    randomString = randomString.replace(re, '');
-    console.log(randomString);
-}
-else if (operation == 3){
-    let symbolIndex = Number(prompt('Enter symbol index'));
-
-    function getRandomIntInclusive(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max); 
-    
-        return Math.floor(Math.random() * (max - min + 1) + min);
+    let str = randomString;
+    let str2 = ``;
+    let str3 = str[symbolIndex-1];
+    console.log(str3);
+    for(let i =0; i<str.length; i++){
+        if(str[i]!==str3){
+            str2 += str[i];
+        } else {
+            str2 += ``;
+        }
     }
+    alert(str2);
+}
+else if (operation === 3){
+    let symbolIndex = Number(prompt('Enter symbol index'));
     const randomInt = String(getRandomIntInclusive(0, 10));
     const symbol = emoticon[randomInt];
 
@@ -37,7 +44,7 @@ else if (operation == 3){
     symbol + randomString.substring(symbolIndex + 1);
     console.log(string)
 }
-else if (operation == 4){
+else if (operation === 4){
     let withoutspace = randomString.replace(/\s/g, '');
     console.log(withoutspace.length);
 }
